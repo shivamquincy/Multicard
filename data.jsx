5dd5cacb6226fa9f7471669e56efd1205a17f20f
shivamquincy <95183060+shivamquincy@users.noreply.github.com>
2023-11-02 09:20:06 +0530

    Add files via upload

---
+++ data.jsx
@@ -0,0 +1,174 @@
+import {GiBrainstorm, GiCompass} from 'react-icons/gi'
+import {SiOpenaigym} from 'react-icons/si'
+import {SiThealgorithms, SiBackendless} from 'react-icons/si'
+import IMG1 from "../src/images1/li2.jpeg"
+
+
+
+
+
+
+
+export const links = [
+    {
+        name1: "Home",
+        path: '/'
+    },
+    {
+        name1: "About",
+        path: '/about'
+    },
+    {
+        name1: "Gallery",
+        path: '/gallery'
+    },
+    {
+        name1: "Plans",
+        path: '/plans'
+    },
+    {
+        name1: "Community",
+        path: '/community'
+    },
+    {
+        name1: "Contact",
+        path: '/contact'
+    }
+]
+export const programs = [
+    {
+        id: 1,
+        image: IMG1,
+        title: "Web Explorers",
+        info: "Crafting Pixels, Shaping Experiences: Our Front-End Wonderland Awaits!",
+        path: "/programs/111",
+        price: "150"
+    },
+    {
+        id: 2,
+        image: IMG1,
+        title: "Algorithmic Invaders",
+        info: "Mastering Algorithms: Join Our Code of Problem Solvers!",
+        path: "/programs/222",
+        price: "99"
+    },
+    {
+        id: 3,
+        image: IMG1,
+        title: "Backend Bulls",
+        info: "Fueling the Digital Backbone: Join Our Backend Community!",
+        path: "/programs/333",
+        price: "200"
+    },
+    {
+        id: 4,
+        image: IMG1,
+        title: "Deep Thinkers",
+        info: "Exploring AI Horizons Together: Welcome to Our Neural Network!",
+        path: "/programs/444",
+        price: "130"
+    },
+    {
+        id: 1,
+        image: IMG1,
+        title: "Web Explorers",
+        info: "Crafting Pixels, Shaping Experiences: Our Front-End Wonderland Awaits!",
+        path: "/programs/111",
+        price: "150"
+    },
+    {
+        id: 2,
+        image: IMG1,
+        title: "Algorithmic Invaders",
+        info: "Mastering Algorithms: Join Our Code of Problem Solvers!",
+        path: "/programs/222",
+        price: "99"
+    },
+    {
+        id: 3,
+        image: IMG1,
+        title: "Backend Bulls",
+        info: "Fueling the Digital Backbone: Join Our Backend Community!",
+        path: "/programs/333",
+        price: "200"
+    },
+    {
+        id: 4,
+        image: IMG1,
+        title: "Deep Thinkers",
+        info: "Exploring AI Horizons Together: Welcome to Our Neural Network!",
+        path: "/programs/444",
+        price: "130"
+    }, {
+        id: 1,
+        image: IMG1,
+        title: "Web Explorers",
+        info: "Crafting Pixels, Shaping Experiences: Our Front-End Wonderland Awaits!",
+        path: "/programs/111",
+        price: "150"
+    },
+    {
+        id: 2,
+        image: IMG1,
+        title: "Algorithmic Invaders",
+        info: "Mastering Algorithms: Join Our Code of Problem Solvers!",
+        path: "/programs/222",
+        price: "99"
+    },
+    {
+        id: 3,
+        image: IMG1,
+        title: "Backend Bulls",
+        info: "Fueling the Digital Backbone: Join Our Backend Community!",
+        path: "/programs/333",
+        price: "200"
+    },
+    {
+        id: 4,
+        image: IMG1,
+        title: "Deep Thinkers",
+        info: "Exploring AI Horizons Together: Welcome to Our Neural Network!",
+        path: "/programs/444",
+        price: "130"
+    }, {
+        id: 1,
+        image: IMG1,
+        title: "Web Explorers",
+        info: "Crafting Pixels, Shaping Experiences: Our Front-End Wonderland Awaits!",
+        path: "/programs/111",
+        price: "150"
+    },
+    {
+        id: 2,
+        image: IMG1,
+        title: "Algorithmic Invaders",
+        info: "Mastering Algorithms: Join Our Code of Problem Solvers!",
+        path: "/programs/222",
+        price: "99"
+    },
+    {
+        id: 3,
+        image: IMG1,
+        title: "Backend Bulls",
+        info: "Fueling the Digital Backbone: Join Our Backend Community!",
+        path: "/programs/333",
+        price: "200"
+    },
+    {
+        id: 4,
+        image: IMG1,
+        title: "Deep Thinkers",
+        info: "Exploring AI Horizons Together: Welcome to Our Neural Network!",
+        path: "/programs/444",
+        price: "130"
+    }
+    
+]
+
+
+
+
+
+
+
+
